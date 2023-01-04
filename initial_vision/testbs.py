187bac67ef097661915612456f6330f1834110f5
pg-7v <dmkseev@gmail.com>
2023-01-04 21:40:53 +0300

    fix

--- initial_vision/testbs.py
+++ initial_vision/testbs.py
@@ -11,6 +11,8 @@ start_time = time.time()
 
 
 
+
+
 # url = 'https://mail.ru'
 # headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36',
 #                'accept': 'text/html',
