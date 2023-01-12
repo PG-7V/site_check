bd64b6375f5b5ab8da2cf59d61fed99abae2cbfa
pg-7v <vasalenovos@gmail.com>
2023-01-12 17:25:15 +0300

    added register page

--- src/urls.py
+++ src/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import keywords_view
+from .views import keywords_view, RegisterView
 
 app_name = 'src'
 
 urlpatterns = [
     path('keywords/', keywords_view, name='keywords'),
+    path('register/', RegisterView.as_view(), name='register'),
+
 ]
--- src/views.py
+++ src/views.py
@@ -1,6 +1,20 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import FormView
+
+from src.forms import RegisterForm
+
 
 @login_required
 def keywords_view(request):
     return render(request, 'src/keywords.html')
+
+class RegisterView(FormView):
+    form_class = RegisterForm
+    template_name = 'registration/register.html'
+    success_url = reverse_lazy("src:keywords")
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
